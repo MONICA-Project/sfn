2398882e171650b8b7821e8465231050b99eda40
hajar <h.sadeghisokeh@kingston.ac.uk>
2018-04-20 14:38:12 +0100

    Amalgamation first commit

--- WP5/KU/SecurityFusionNodeService/SecurityFusionNode/security_fusion_node.py
+++ WP5/KU/SecurityFusionNodeService/SecurityFusionNode/security_fusion_node.py
@@ -4,6 +4,58 @@ import cv2
 import json
 import base64
 
+def rotateImage(image, angle):
+    """
+    Rotates the given image about it's centre
+    """
+
+    image_size = (image.shape[1], image.shape[0])
+    image_center = tuple(np.array(image_size) / 2)
+
+    rot_mat = np.vstack([cv2.getRotationMatrix2D(image_center, angle, 1.0), [0, 0, 1]])
+    trans_mat = np.identity(3)
+
+    w2 = image_size[0] * 0.5
+    h2 = image_size[1] * 0.5
+
+    rot_mat_notranslate = np.matrix(rot_mat[0:2, 0:2])
+
+    tl = (np.array([-w2, h2]) * rot_mat_notranslate).A[0]
+    tr = (np.array([w2, h2]) * rot_mat_notranslate).A[0]
+    bl = (np.array([-w2, -h2]) * rot_mat_notranslate).A[0]
+    br = (np.array([w2, -h2]) * rot_mat_notranslate).A[0]
+
+    x_coords = [pt[0] for pt in [tl, tr, bl, br]]
+    x_pos = [x for x in x_coords if x > 0]
+    x_neg = [x for x in x_coords if x < 0]
+
+    y_coords = [pt[1] for pt in [tl, tr, bl, br]]
+    y_pos = [y for y in y_coords if y > 0]
+    y_neg = [y for y in y_coords if y < 0]
+
+    right_bound = max(x_pos)
+    left_bound = min(x_neg)
+    top_bound = max(y_pos)
+    bot_bound = min(y_neg)
+
+    new_w = int(abs(right_bound - left_bound))
+    new_h = int(abs(top_bound - bot_bound))
+    new_image_size = (new_w, new_h)
+
+    new_midx = new_w * 0.5
+    new_midy = new_h * 0.5
+
+    dx = int(new_midx - w2)
+    dy = int(new_midy - h2)
+
+
+    # getTranslationMatrix2d: a numpy affine transformation matrix for a 2D translation of
+    trans_mat = np.matrix([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+    affine_mat = (np.matrix(trans_mat) * np.matrix(rot_mat))[0:2, :]
+    result = cv2.warpAffine(image, affine_mat, new_image_size, flags=cv2.INTER_LINEAR)
+
+    return result
+
 
 class SecurityFusionNode:
 
@@ -100,5 +152,47 @@ class SecurityFusionNode:
         # TODO: THIS WILL LIKELY NEED CHANGING IN ORDER TO COMPRESS THESE IMAGES
         return heat_map.tolist(), heat_image
 
-    def new_function(self):
-        print('HERE')
+    @staticmethod
+    def generate_amalgamated_topDown_map(topDown_maps, config_for_amal):
+        """
+        Amalgamate N color images from a list of image paths.
+        """
+        x = []
+        y = []
+        w = []
+        h = []
+        all_rotated_images = []
+        for i in range(len(topDown_maps)):
+            # Convert from list of lists to matrix
+            img = np.array(topDown_maps[i])
+            print(img.shape)
+            rotated_img = rotateImage(img, config_for_amal[i][2])  # angle
+
+            all_rotated_images.append(rotated_img)
+            x.append(config_for_amal[i][0])  # latitude
+            y.append(config_for_amal[i][1])  # longitude
+            w.append(rotated_img.shape[0])
+            h.append(rotated_img.shape[1])
+
+        amal_latitude = min(x)
+        amal_longitude = min(y)
+
+        amal_w = int(max([a + b for a, b in zip(x, w)]) - amal_latitude)
+        amal_h = int(max([a + b for a, b in zip(y, h)]) - amal_longitude)
+
+        # Generate the amalgamated image
+        img_amal = np.zeros(shape=(amal_w, amal_h))
+
+        for i in range(len(all_rotated_images)):
+            # y[3:8, 1:3] = X + y[3:8, 1:3] * (X == 0)
+            img_amal[int(x[i] - amal_latitude):int(x[i] - amal_latitude + w[i]),
+            int(y[i] - amal_longitude):int(y[i] - amal_longitude + h[i])] = all_rotated_images[i] + \
+                                                                            img_amal[int(x[i] - amal_latitude):int(
+                                                                                x[i] - amal_latitude + w[i]),
+                                                                            int(y[i] - amal_longitude):int(
+                                                                                y[i] - amal_longitude + h[i])] * (
+                                                                                all_rotated_images[i] == 0)
+
+        cv2.imshow('img_amal', img_amal)
+        cv2.waitKey(0)
+        return img_amal
