049a798e2424170c21b93ac04ba6115c1d494e9a
Hajar <h.sadeghisokeh@kingston.ac.uk>
2018-06-05 15:39:57 +0100

    Adding flow analysis module

--- WP5/KU/Algorithms/flow_analysis/get_flow.py
+++ WP5/KU/Algorithms/flow_analysis/get_flow.py
@@ -2,50 +2,119 @@
 import numpy as np
 import pickle
 from WP5.KU.Algorithms.frame_analyser import FrameAnalyser
+from WP5.KU.Algorithms.flow_analysis.FlowNet2_src.flow_2img import flow_2img
 import json
 import cv2
-
+import math
+import datetime
+from WP5.KU.Algorithms.flow_analysis.FlowNet2_src import flow_to_image
 
 __version__ = '0.1'
 __author__ = 'Hajar Sadeghi (KU)'
 
 
-class GetFlow(FrameAnalyser):
 
-    def __init__(self, module_id, settings_location):
+
+class GetFlow(FrameAnalyser):
+    def __init__(self, module_id):  # def __init__(self, module_id, settings_location):
         """ Initialise the GetFlow object.
             Keyword arguments:
                 module_id -- A unique string identifier
-                settings_location -- Camera settings: location
+                settings_location -- Module settings: location
         """
         self.module_id = module_id + '_flow'
         self.type_module = 'flow'
         self.state = True
+        self.previous_frames_dictionary = {}  # a dictionary of (camera_id,frame) pair
         FrameAnalyser.__init__(self, module_id)
         # CAMERA INFO
         self.roi = [0, 300, 0, 300]
         self.cam_id = ''
         self.weights_path = []
-        self.load_settings(settings_location)
+        # self.load_settings(settings_location)
 
         # ALGORITHM VARIABLES
         self.cuda = False
+        self.scale_height = 384  # TO SCALE THE INPUT IMAGE IF IT IS TOO LARGE FOR FLOWNET
+        self.scale_width = 512
 
-    def process_frame(self, frame, camera_id):
+    def process_frame(self, frame, camera_id, roi, rois):  # rois: region of interests
+        # CHECK WHETHER THIS IS THE FIRST FRAME OF THIS CAMERA ID
+        if camera_id not in self.previous_frames_dictionary:
+            self.previous_frames_dictionary[camera_id] = frame
+            message = self.create_obs_message([], [], datetime.datetime.utcnow().isoformat())
+            return (message, [])
+
+
         # USES ONLY THE REGION OF INTEREST DEFINED IN THE SETTINGS
-        frame = frame[self.roi[1]:self.roi[3], self.roi[0]:self.roi[2], :]
-        # CONVERT FRAME TO WHATEVER FORMAT IS REQUIRED
+        frame1 = self.previous_frames_dictionary[camera_id]
+        frame2 = frame
+        self.previous_frames_dictionary[camera_id] = frame2
 
-        # ADD INFERENCE CODE FOR THE FRAME HERE
+        frame1 = frame1[roi[1]:roi[3], roi[0]:roi[2], :]
+        frame2 = frame2[roi[1]:roi[3], roi[0]:roi[2], :]
 
-        # CREATE THE MESSAGE AND RETURN
+        height, width = frame1.shape[:2]
+
+        # DO SOME SCALE TO OPTIMAL MODEL INPUT, MAYBE SPLIT IMAGE IF ITS TOO LARGE?
+        fr1 = cv2.resize(frame1, (self.scale_height, self.scale_width))
+        # cv2.resize(frame1, (0, 0), fx=self.scale, fy=self.scale)
+        fr2 = cv2.resize(frame2, (self.scale_height, self.scale_width)) # cv2.resize(frame2, (0, 0), fx=self.scale, fy=self.scale)
+
+        flow_uv = flow_2img(fr1, fr2)
+
+        # CONVERT BACK TO ORIGINAL SCALE
+        flow_uv = cv2.resize(flow_uv, (width, height))
+
+        # vISUALIZE THE OPTICAL FLOW AND SAVE IT
+        flow_image = flow_to_image(flow_uv)
+
+        ave_flow_mag = []
+        ave_flow_dir =[]
+        for i in range(len(rois)):
+            roi_current = rois[i]
+            flow_uv_current = flow_uv[roi_current[1]:roi_current[3], roi_current[0]:roi_current[2], :]
+
+            mean_u = flow_uv_current[:, :, 0].mean()
+            mean_v = flow_uv_current[:, :, 1].mean()
+
+            mag = math.sqrt(math.pow(mean_u, 2) + math.pow(mean_v, 2))
+
+            if mean_v < 0:
+                uv_angle = 360 + math.degrees(math.atan2(mean_v, mean_u))
+            else:
+                uv_angle = math.degrees(math.atan2(mean_v, mean_u))
+            direction = uv_angle / 360
+
+            ave_flow_mag.append(mag)
+            ave_flow_dir.append(direction)
+
+        # CREATE THE MESSAGE
         self.cam_id = camera_id
-        message = self.create_obs_message()
-        return message, frame
+        message = self.create_obs_message(ave_flow_mag, ave_flow_dir, datetime.datetime.utcnow().isoformat())
 
-    def create_obs_message(self):
-        # ADD VARIABLES TO BE DUMPED INTO JSON
-        data = {'count': int(0), 'heat_map': np.zeros([5, 5])}
+        return message, flow_image
+
+    def create_obs_message(self, average_flow_mag, average_flow_dir, timestamp):
+        """ Function to create the JSON payload containing the observation. Follows the content as defined on the WP5
+        Confluence (6. JSON Message Format):
+            count --        Double defining the total number of people in the frame's roi
+            density_map --  The output of the algorithm
+            timestamp --    the time the frame is processed
+            frame --        [OPTIONAL] the input frame given to the algorithms process after the roi is applied
+        Returns:
+            message --      the JSON message payload
+        """
+        data = {
+            'module_id': self.module_id,
+            'type_module': self.type_module,
+            'camera_ids': [self.cam_id],
+            'average_flow_magnitude': average_flow_mag,
+            'average_flow_direction': average_flow_dir,
+            'flow_frame_byte_array': '',
+            'timestamp': timestamp,
+        }
+
         message = json.dumps(data)
         return message
 
@@ -57,7 +126,6 @@ class GetFlow(FrameAnalyser):
                 'state': self.state,
         }
         message = json.dumps(data)
-        # message = json.loads(message)
         return message
 
     def load_settings(self, settings_location):
