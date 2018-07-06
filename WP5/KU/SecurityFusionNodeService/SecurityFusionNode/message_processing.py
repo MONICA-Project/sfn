36cef511bd8ee40fad03639ffb3ad88413b30e80
Rob Dupre <r.dupre@kingston.ac.uk>
2018-07-06 12:21:22 +0100

    Added some more debug prints

--- WP5/KU/SecurityFusionNodeService/SecurityFusionNode/message_processing.py
+++ WP5/KU/SecurityFusionNodeService/SecurityFusionNode/message_processing.py
@@ -176,6 +176,7 @@ def amalgamate_crowd_density_local(sfn_instance, url):
                     break
 
         if config is not None:
+            print(config['camera_id'])
             print(config['ground_plane_position'])
             print(config['camera_bearing'])
             config_for_amalgamation.append(config['ground_plane_position'] + [config['camera_bearing']])
