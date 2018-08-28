1229e20b8f132130201c06b44cc1c8ab8ed3986f
Rob Dupre <r.dupre@kingston.ac.uk>
2018-08-28 17:15:10 +0100

    Added extra cameras to registration script

--- WP5/KU/SecurityFusionNodeService/tivoli_reg.py
+++ WP5/KU/SecurityFusionNodeService/tivoli_reg.py
@@ -37,6 +37,8 @@ configs = [
     tools.load_settings(os.path.join(KU_DIR, 'KUConfigTool/cam_configs/'), 'TIVOLI_27'),
     tools.load_settings(os.path.join(KU_DIR, 'KUConfigTool/cam_configs/'), 'TIVOLI_28'),
     tools.load_settings(os.path.join(KU_DIR, 'KUConfigTool/cam_configs/'), 'TIVOLI_29'),
+    tools.load_settings(os.path.join(KU_DIR, 'KUConfigTool/cam_configs/'), 'TIVOLI_30'),
+    tools.load_settings(os.path.join(KU_DIR, 'KUConfigTool/cam_configs/'), 'TIVOLI_31'),
     # tools.load_settings(os.path.join(KU_DIR, 'Algorithms/registration_messages/'), '1234sdfv234jk_action_recognition_reg', False),
 ]
 
